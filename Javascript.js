301565c33e8867511ab5e1b728bc56fb2eb212c8
Lionblast <58056257+Lionblast@users.noreply.github.com>
2019-12-05 20:14:46 -0800

    December 5th's Java Script code

---
+++ Javascript.js
@@ -0,0 +1,51 @@
+var hello="<h1>Hello life</h1>";
+
+
+/*
+var n10000 = 1
+while(n10000 < 10001 ) {
+    document.write(hello);
+    n10000 += 1;
+}
+**/
+/*
+for(var n10000 = 1; n10000 <= 10000; n10000++){
+   if(n10000 %2 == 0 ) {
+    document.write("<div>"+"<h1>" + n10000 + "</h1>"+ "</div>");
+    document.write("<br>");
+    }
+   }
+  */  
+ /*
+ for(var n10000 = 0; n10000 <= 1000; n10000+=1/3){
+    
+     document.write("<div>"+"<h1>" + n10000 + "</h1>"+ "</div>");
+     document.write("<br>");
+     
+    }
+*/
+/*
+count("Club Penguin Gang")
+function count (number) {
+    for(var n10000 = 0; n10000 <= 1000; n10000+=2){
+    
+        document.write("<div>"+"<h1>" + number + "</h1>"+ "</div>");
+        document.write("<br>");
+        
+       }
+}
+*/ /*
+function expo(num1, num2) {
+    return num1 ** num2
+}
+
+var solution = expo(10,2);
+document.write(solution);
+solution = expo(solution, 2);
+*/
+
+function add(first, second) {
+    return first + second
+}
+
+var sol = add(1+1);
---
+++ Stack.js
@@ -0,0 +1,60 @@
+//creates an emptly aray or stack// 
+var stack = [];
+stack.push("Pancake 1");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+
+stack.push("Pancake 2");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+stack.push("Pancake 3");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+stack.push("Pancake 4");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+stack.push("Pancake 5");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+stack.push("Pancake 6");
+document.write(stack);
+document.write("<br>");
+document.write("<br>");
+
+
+stack.pop ();
+stack.pop();
+document.write(stack);
+document.write("<br>");
+
+
+// Create a queue //
+var queue = [];
+
+// add to end of queue // 
+
+queue.push("Person1")
+document.write(queue);
+document.write("<br>");
+
+queue.push("Person2")
+document.write(queue);
+document.write("<br>");
+
+queue.push("person3")
+document.write(queue);
+document.write("<br>");
+
+// Take the first person in line // 
+queue.shift();
+document.write(queue);

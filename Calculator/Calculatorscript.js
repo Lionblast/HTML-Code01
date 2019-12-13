fbed9cf574d1a3a5379162f8f05ae5fb4d59acfe
Lionblast <58056257+Lionblast@users.noreply.github.com>
2019-12-12 20:14:58 -0800

    Calculator

---
+++ Calculator/Calculatorscript.js
@@ -0,0 +1,70 @@
+class Calculator {
+    display = document.getElementsByClassName('screen')[0];
+    stack = [];
+
+    clear() {
+        //This function clears the screen//
+        this.display.value ='0';
+    }
+    enter() {
+
+        this.stack.push(this.display.value);
+        this.clear();
+    }
+    numberHandel(number) {
+        if( this.display.value ==="0" ) {
+           this.display.value ="" 
+        }
+        this.display.value += String(number);
+    }
+        
+
+    operatorHandel(operator) {
+var value1 = (this.stack.pop());
+var value2 = (this.stack.pop());
+        if(operator === "*")
+            this.display.value = String(value1*value2);
+            else if ( operator === "/")
+                this.display.value = String(psrseInt(value1)/psrseInt(value2));
+            else if ( operator === "+")
+                    this.display.value = String(psrseInt(value1)+psrseInt(value2));
+            else if ( operation === "-")
+                this.display.value = String(psrseInt(value1)-psrseInt(value2));
+        
+
+    }
+    calculate() {
+        document.addEventListener('click', (event) => {
+
+            const {
+                target
+            } = event;
+
+            if (target.classList.contains('number')) {
+                this.numberHandel(target.value);
+            } else if (target.classList.contains('operator')) {
+
+                this.operatorHandel(target.value);
+
+            } else if (target.classList.contains('clear')) {
+
+                this.clear();
+
+            } else if (target.classList.contains('enter')) {
+
+            this.enter();
+
+            }
+        });
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    //created a new calculator object//
+    calculator = new Calculator();
+    //Used a function from this object//
+    calculator.clear();
+    calculator.calculate();
+
+});
